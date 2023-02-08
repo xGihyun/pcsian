6e69dad985961e65d1fc6285b92ed40b76f503f7
Gihyun <giordnnuz@gmail.com>
2023-02-08 14:54:39 +0800

    Test deploy on Render (6)

--- .netlify/server/nodes/10.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 10;
-export const component = async () => (await import('../entries/pages/(about)/services/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/(about)/services/_page.svelte-94b0c3d5.js';
-export const imports = ["_app/immutable/components/pages/(about)/services/_page.svelte-94b0c3d5.js","_app/immutable/chunks/index-87b088c1.js","_app/immutable/chunks/index-325b4d75.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .netlify/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/(about)/_layout.svelte.js')).default;
-export const file = '_app/immutable/components/pages/(about)/_layout.svelte-c554f872.js';
-export const imports = ["_app/immutable/components/pages/(about)/_layout.svelte-c554f872.js","_app/immutable/chunks/index-87b088c1.js","_app/immutable/chunks/TopHeading-59a24299.js"];
-export const stylesheets = [];
-export const fonts = [];
